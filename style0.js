0f8ef0d17fe72f50e2ecd24eaf76edbc1ac9085d
Rishi Raj Sisodia <rishirsisodia@gmail.com>
2019-10-16 02:37:16 +0530

    Final Finishing

---
+++ style0.js
@@ -0,0 +1,10 @@
+var play=document.getElementById('play');
+var resetscore=document.getElementById('play1');
+play.onclick=()=>{
+    location.href="play.html";
+}
+
+resetscore.onclick=()=>{
+    localStorage.setItem('hscore',0);
+    alert("HighScore has been Reset!");
+}
